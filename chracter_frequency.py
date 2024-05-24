4032f9886f2ba627bf359491ac5f233985d2217c
KhaingLwinThant <160337642+KhaingLwinThant@users.noreply.github.com>
2024-05-25 02:14:54 +0630

    git commit

--- chracter_frequency.py
+++ chracter_frequency.py
@@ -1,7 +1,8 @@
 def char_frequency(user_input):
     frequency_dict = {}
     for char in user_input:
-        frequency_dict[char] = frequency_dict.get(char, 0) + 1
+        if char != ' ':
+            frequency_dict[char] = frequency_dict.get(char, 0) + 1
     return frequency_dict
 
 user_input = input('Enter a string: ')
